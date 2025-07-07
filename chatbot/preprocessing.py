edf740d148f1375d869c775614b9146d537e98d9
AntoMiele <antonio.miele10@gmail.com>
2025-07-07 17:22:02 +0200

    Ho aggiunto un commento

--- chatbot/preprocessing.py
+++ chatbot/preprocessing.py
@@ -6,3 +6,4 @@ def preprocess_documents(data_path):
         with open(os.path.join(data_path, filename), 'r', encoding='utf-8') as f:
             docs.append(f.read())
     return docs
+#ciaooeao0rfdsi<okvocdkms
